4dd4793a8278fba31f503bd962ff7a9706af6bd1
Jaume Planas <jaume.planas@minorisa.net>
2019-01-17 10:20:17 +0100

    [11.0] Tests

---
+++ {{ cookiecutter.module_name }}/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2011 Minorisa, S.L. <http://www.minorisa.net>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import test_module
---
+++ {{ cookiecutter.module_name }}/tests/test_module.py
@@ -0,0 +1,13 @@
+# Copyright 2011 Minorisa, S.L. <http://www.minorisa.net>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestModelAccountMove(TransactionCase):
+
+    def setUp(self):
+        pass
+
+    def test_partial_field(self):
+        pass
