22fbc50a9bb5a41bb75fb46598a78d797dbdf31c
Ramon Guiu <guiu33@hotmail.com>
2022-02-25 12:05:19 +0100

    Create __manifest__.py

---
+++ {{ cookiecutter.module_name }}/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2011 Minorisa, S.L. <http://www.minorisa.net>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "{{ cookiecutter.module_title }}",
+    "category": "",
+    "summary": "{{ cookiecutter.module_summary }}",
+    "website": "https://www.minorisa.net",
+    "version": "14.0.0.1.0",
+    "author": "{{cookiecutter.author}}",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        # Security
+        # "security/ir.model.access.csv",
+        # Views
+        # "views/module_views.xml",
+        # Data
+        # Reports
+        # Wizards
+    ],
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
