16f76b2a8c4e12ea12fb373affdbc68848bf00eb
Ramon Guiu <guiu33@hotmail.com>
2022-02-25 12:17:01 +0100

    Add files via upload

---
+++ {{ cookiecutter.module_name }}/test/test_sample.py
@@ -0,0 +1,13 @@
+# Copyright 2011-now Minorisa, S.L. <http://www.minorisa.net>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.tests import common
+
+
+class TestSample(common.TransactionCase):
+
+    def setUp(self):
+        super(TestSample, self).setUp()
+
+    def test_sample(self):
+        pass
