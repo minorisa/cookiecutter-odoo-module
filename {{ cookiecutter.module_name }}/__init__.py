9b963650610a9efbff3056516ece6cf29a74b5fb
Ramon Guiu <guiu33@hotmail.com>
2022-02-25 12:01:51 +0100

    Update __init__.py
    [FIX] update 14.0

--- {{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.module_name }}/__init__.py
@@ -1,23 +1,7 @@
-##############################################################################
-#
-#    Copyright (c) 2011-TODAY MINORISA (http://www.minorisa.net)
-#                             All Rights Reserved.
-#                             Minorisa <contact@minorisa.net>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
+# Copyright 2011-now Minorisa, S.L. <http://www.minorisa.net>
+# License AGPL-3.0 or later
+# (https://www.gnu.org/licenses/licenses.en.html#AGPL).
 
 from . import models
 from . import wizards
